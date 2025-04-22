912a9b5179cb3cef6a821714ce6ac6f6be310b08
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-22 13:51:44 +0500

    fgfd

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,7 +7,7 @@ const Main = () => {
     <>
       <div
         className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-        style={{ backgroundImage: "url('src/assets/header_img.png')" }}
+        style={{ backgroundImage: "url('/header_img.png')" }}
       >
         <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
           <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20 font-Outfit animate__bounce ">
