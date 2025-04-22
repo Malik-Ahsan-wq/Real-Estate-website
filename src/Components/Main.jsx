8afd74dc9b06e798dab44397cf7f4d9245e9dd67
Malik-Ahsan-wq <ahsanmalikking57@gmail.com>
2025-04-22 13:59:46 +0500

    dfsd

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -7,8 +7,8 @@ const Main = () => {
     <>
       <div
         className="min-h-screen mb-4 bg-cover bg-center flex items-center w-full overflow-hidden"
-        style={{ backgroundImage: "url('/header_img.png')" }}
       >
+        <img src="/header_img.png" alt="" />
         <div className="container text-center mx-auto py-4 px-6 md:px-20 lg:px-32 text-white">
           <h2 className="text-5xl sm:text-6xl md:text-[82px] inline-block max-w-3xl font-semibold pt-20 font-Outfit animate__bounce ">
             Explore homes that fit your dreams
